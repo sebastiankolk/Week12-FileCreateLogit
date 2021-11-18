63e19a767f50b55542c0181605ee6427cd72b710
Opilane TTHK <opilane@tthk.ee>
2021-11-18 10:12:32 +0200

    fucku

---
+++ ToDoList/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ToDoList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> toDoFromFile = new List<string>();
+            string toDoFilePath = $@"C:\Users\opilane\samples\titpe21\ToDoList.txt";
+            toDoFromFile = File.ReadAllLines(toDoFilePath).ToList();
+
+            foreach (string task in toDoFromFile)
+            {
+                Console.WriteLine(task);
+            }
+
+            bool loopActive = true;
+
+            while (loopActive)
+            {
+                Console.WriteLine("Would you like to add a task? Y/N");
+                char userInput = Convert.ToChar(Console.ReadLine().ToLower());
+
+                if (userInput == 'y')
+                {
+                    Console.WriteLine("Enter a task:");
+                    string userTask = Console.ReadLine();
+                    toDoFromFile.Add(userTask);
+                    Console.WriteLine($"Task {userTask} added.");
+                }
+                else
+                {
+                    Console.WriteLine("Take care!");
+                    loopActive = false;
+                }
+            }
+
+            File.WriteAllLines(toDoFilePath, toDoFromFile.ToArray());
+
+        }
+    }
+}
---
+++ createLogitpe21/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+namespace createLogitpe21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string directoryName = "LOGITpe21";
+            CreateMyDirectory(directoryName);
+        }
+
+        private static void CreateMyDirectory(string myDirectoryName)
+        {
+            string rootDirectory = @"C:\Users\opilane\samples";
+            string directoryPath = $@"{rootDirectory}\{myDirectoryName}.";
+            if (Directory.Exists(directoryPath))
+            {
+                Console.WriteLine($"Directory {myDirectoryName} already exists in {directoryPath}");
+            }
+            else
+            {
+                Directory.CreateDirectory(directoryPath);
+                Console.WriteLine($"Directory {myDirectoryName} has been created");
+            }
+
+
+        }
+    }
+}
